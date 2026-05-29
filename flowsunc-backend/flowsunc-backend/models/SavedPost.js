b55654ac19a95818e6d485d710f07101de7438eb
Rahul.pure <purerahul630@gmail.com>
2026-05-29 12:10:40 +0530

    Fix backend folder typo and delete duplicate folder

--- flowsunc-backend/flowsunc-backend/models/SavedPost.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const savedPostSchema = new mongoose.Schema({
-  imageId: { type: mongoose.Schema.Types.ObjectId, ref: 'Image' },
-  userId:  { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-}, { timestamps: true });
-
-module.exports = mongoose.model('SavedPost', savedPostSchema);
